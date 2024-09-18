c9806bc471b2ee1b5cdc3cf5a0a196e98f435522
Vitorverg97 <vitor.vsouza05@gmail.com>
2024-09-18 18:01:04 +0000

    criamos algoritmo boletim

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# coding1
+# Resumo aula coding 01 11-09-2024 
+
+No dia de hoje foi apresentado pela professora: como podemos operar variáveis em um arquivo .js, explicando detalhadamente sobre os tipos aplicáveis de variáveis (var, let, const), além disso foi mostrado a concatenação de palavras (soma de palavras) e a utilização das funções: Soma, subtração, multiplicação e divisão.
+
+# Resumo aula coding 18-09-2024
+
+Nesta aula foi nos apresentado como podemos utilizar a função média, comandos do github (LS status, git status, git add) e a utlização de um arquivo .gitignore no vscode online.
---
+++ aula.js
@@ -0,0 +1,42 @@
+//Este código foi para treino de variáveis.
+
+/*const id = "Edilene";
+id = 12345;
+console.log(id);
+*/
+
+
+// Concatenando nomes
+/*
+let Myname = "Creusa";
+console.log('Olá'+' '+Myname+'!')
+*/
+
+/*
+let myName2 = "Viana"
+console.log(`Olá ${myName2}`+'!')
+*/
+
+// Função soma, subtração, multiplicação, divisão
+
+/*
+const numero1 = 100;
+const numero2 = 10;
+const soma = numero1+numero2;
+const subtracao = numero1 - numero2;
+const multiplicacao  = numero1 * numero2;
+const divisao = numero1/numero2;
+
+console.log("O resultado da soma é: " + (soma));
+console.log("O resultado da subtração é: " + (subtracao));
+console.log("O resultado da multiplicação é: " + (multiplicacao));
+console.log("O resultado da divisão é: " + (divisao));
+*/
+
+const nota1 = 9;
+const nota2 = 6;
+const nota3 = 10;
+const media = (nota1 + nota2 + nota3)/3;
+
+console.log(`Sua média é: ${media}`)
+
